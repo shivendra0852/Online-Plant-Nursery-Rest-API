9346a7ae8f398922862784d6e372ea5b5bbfa1c3
rawatshubham1645 <rawatshubham1645@gmail.com>
2023-01-19 23:54:45 +0530

    commited

---
+++ GreenHood/src/main/java/com/greenhood/model/Seed.java
@@ -0,0 +1,15 @@
+package com.greenhood.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Seed {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer seedId;
+	
+}
